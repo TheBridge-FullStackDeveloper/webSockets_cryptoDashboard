3621d4e8d8cf47849de72e3402d7204b5b3f73f5
borj4 <river4car@gmail.com>
2022-02-22 15:38:14 +0100

    add api

--- src/App.js
+++ src/App.js
@@ -5,30 +5,97 @@ import { io } from "socket.io-client";
 
 
 const App = () => {
-  const [info, setInfo] = useState(false)
+  const [info, setInfo] = useState(false);
+  const [currencies, setcurrencies] = useState([]);
+
+
+  const url = "https://api.pro.coinbase.com";
+  let pairs = [];
 
   
   useEffect(()=>{ // maneja la primera petición al servidor
     
+    // peticion a la api
+    const apiCall = async ()=> {
+      // peticion
+      await fetch(url + "/products")
+      .then((res) => res.json())
+      .then((data) => (pairs = data));
+      console.log(pairs)
+      
+      // filtrar las criptos cruzadas con el euro
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "EUR") {
+          return pair;
+        }
+      });
+      console.log(filtered)
+
+      
+      // ordenar el array filtrado de criptos en base a su valor
+      filtered = filtered.sort((a, b) => {
+        if( a.base_currency == b.base_currency ) { return 0; }
+        else return ( a.base_currency > b.base_currency ? 1 : -1 )
+    });
+    console.log(filtered)
+    
+
+    setcurrencies(filtered);
+    
+    // first.current = true;
+
+    
+
+
+  };
+  apiCall();
+
+
     // configuración principal del WebSocket
     const socket = io.connect("wss://ws-feed.pro.coinbase.com");
-    socket.on("connect", () => {
-        
+    socket.on("connect", (data) => {
+      console.log(`is my socket connected? +${socket.connected} \n the id is ${socket.id}`);
+      console.log(data)
     });
     
+    // pedir suscripción al servidor
+    let msg = 
+    {
+      "type": "subscribe",
+      "product_ids": [
+          "ETH-USD",
+          "ETH-EUR"
+      ],
+      "channels": [
+          "level2",
+          "heartbeat",
+          {
+              "name": "ticker",
+              "product_ids": [
+                  "ETH-BTC",
+                  "ETH-USD"
+              ]
+          }
+      ]
+    };
+
+    // esto es lo que tiene el ejemplo:
+    // {
+    // type: "subscribe",
+    // product_ids: [pair],
+    // channels: ["ticker"]
+    // };
+
+    let jsonMsg = JSON.stringify(msg);
+    
+    // de aquí, una u otra
+    // socket.on("message", jsonMsg);
+    socket.send(jsonMsg);
 
   },[])
 
   // useEffect(()=>{
 
-  //   // pedir suscripción al servidor
-  //   let msg = {
-  //     type: "subscribe",
-  //     product_ids: [pair],
-  //     channels: ["ticker"]
-  //   };
-  //   let jsonMsg = JSON.stringify(msg);
-  //   socket.send(jsonMsg);
 
     
   //   // procesar la entrada de datos del socket
