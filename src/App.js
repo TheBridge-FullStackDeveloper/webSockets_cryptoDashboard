abf48ba0237e69ee2d342323bf090290c1eadf65
borj4 <river4car@gmail.com>
2022-03-04 13:37:56 +0100

    update: select

--- src/App.js
+++ src/App.js
@@ -4,31 +4,18 @@ import Footer from './components/footer/footer';
 import { io } from "socket.io-client";
 
 const App = () => {
-  const [info, setInfo] = useState(false);
+  const [info, setInfo] = useState("I don't know");
   const [currencies, setCurrencies] = useState([]);
   const [pair, setPair] = useState("");
   const [price, setprice] = useState("0.00");
 
-  
+  // así se setea la direccion a la que pedir el socket
   const socket = io("wss://ws-feed.exchange.coinbase.com");
 
   const url = "https://api.pro.coinbase.com";
   let first = useRef(false);
-
-
-  // opcion 2 que no funciona
-  // const socket = io("https://api.pro.coinbase.com", {
-  //   withCredentials: true,
-  //   mode: 'cors',
-  //   credentials: 'include',
-  //   extraHeaders: {
-  //     'Access-Control-Allow-Credentials': 'true',
-  //   }
-  // });
-
-  // const url = "https://api.pro.coinbase.com";
-  let pairs = [];
   let flag = useRef(false);
+  let pairs = [];
 
   ///////////////////////////
   // SERVER'S MESSAGE REQUEST
@@ -42,7 +29,7 @@ const App = () => {
         "heartbeat",
         {
             "name": "ticker",
-            "product_ids": ["ETH-BTC","ETH-USD"]
+            "product_ids": [pair]
         }
     ]
   };
@@ -53,62 +40,47 @@ const App = () => {
   }
   
   useEffect(()=>{ // maneja la primera petición al servidor
-  console.log("entra 1")
-  ////////////////////
-  // SOCKET CONNECTION
-  ////////////////////
+    
+    console.log("entra 1");
 
-    // forma 1
-    // socket = io.connect("wss://ws-feed.exchange.coinbase.com")
-    // ("Access-Control-Allow-Origin",{
-    // // mis problemas con cors
-    //   cors: {
-    //     origin: "http://localhost:3000",
-    //     methods: ["GET", "POST"]
-    //   }
-    // });
-
-    // forma 2
-    // socket = io.connect("wss://ws-feed.exchange.coinbase.com")
-
-    // forma 3
+    ////////////////////
+    // SOCKET CONNECTION
+    ////////////////////
     socket.on("connect", () => {
       console.log(socket.id); // x8WIv7-mJelg7on_ALbx
     });
 
-  //////////////
-  // API REQUEST
-  //////////////
-  const apiCall = async ()=> {
+    //////////////
+    // API REQUEST
+    //////////////
+    const apiCall = async ()=> {
 
-    // seteamos la primera petición al webSocket
+      // peticion
+      await fetch(url + "/products")
+      .then((res) => res.json())
+      .then((data) => (pairs = data));
+      console.log(pairs)
 
-    // peticion
-    await fetch(url + "/products")
-    .then((res) => res.json())
-    .then((data) => (pairs = data));
-    console.log(pairs)
-    
-    // filtrar las criptos cruzadas con el euro
-    let filtered = pairs.filter((pair) => {
-      if (pair.quote_currency === "EUR") {
-        return pair;
-      }
-    });
+      // filtrar las criptos cruzadas con el euro
+      let filtered = pairs.filter((pair) => {
+        if (pair.quote_currency === "EUR") {
+          return pair;
+        }
+      });
 
-    // ordenar por nombre el array filtrado de criptos
-    filtered = filtered.sort((a, b) => {
-      if( a.base_currency == b.base_currency ) { return 0; }
-      else return ( a.base_currency > b.base_currency ? 1 : -1 )
-    });
+      // ordenar por nombre el array filtrado de criptos
+      filtered = filtered.sort((a, b) => {
+        if( a.base_currency == b.base_currency ) { return 0; }
+        else return ( a.base_currency > b.base_currency ? 1 : -1 )
+      });
 
-    // console.log(filtered)
-    setCurrencies(filtered);
-    
-    // evitar la conexion al socket si no hay un fetch previo
-    flag.current = true;
-  };
-  apiCall();
+      // console.log(filtered)
+      setCurrencies(filtered);
+
+      // evitar la conexion al socket si no hay un fetch previo
+      flag.current = true;
+    };
+    apiCall();
 
   },[]) // render en la primera carga
 
@@ -116,72 +88,67 @@ const App = () => {
   useEffect(()=>{
     console.log("entra 2")
 
-    // esto evita peticionar al socket si no hay fetch previo
-    // if (!flag.current) {
-    //   return;
-    // }
-
     let jsonMsg = JSON.stringify(subMsg);
     // socket.on("message", jsonMsg);
     socket.emit(jsonMsg);
-    // configuración principal del WebSocket
-    // socket.on("connect", (data) => {
-    //   console.log(`is my socket connected? +${socket.connected} \n the id is ${socket.id}`);
-    //   console.log(data)
-    // });
+    
     
     //////////////////////
     // EXTRACT SOCKET DATA
     //////////////////////
     socket.on("connect", (e) => {
       let data = JSON.parse(e.data);
-      setprice(data.price);
-
-      // if (data.type !== "ticker") {
-      //   return;
-      // }
-
-      // if (data.product_id === pair) {
-      //   setprice(data.price);
-      // }
+      console.log(data);
+      // setprice(data.price);
+      if (data.type !== "ticker") {
+        return;
+      }
+      if (data.product_id === pair) {
+        setprice(data.price);
+      }
     });
+    
+    socket.on("connect", () => {
+      console.log(socket.connected);
+    });
+    
+    /////////////////////////////////////////////////////////////////////////////////////////
+    // esto te trae la información anterior a la última cotización para alimientar la gráfica
+    /////////////////////////////////////////////////////////////////////////////////////////
 
-    // no tengo ni idea de qué hace esto
     // let historicalDataURL = `${url}/products/${pair}/candles?granularity=86400`;
     // const fetchHistoricalData = async () => {
     //   let dataArr = [];
     //   await fetch(historicalDataURL)
     //     .then((res) => res.json())
     //     .then((data) => (dataArr = data));
-      
+    
     //   let formattedData = formatData(dataArr);
     //   setpastData(formattedData);
     // };
     // fetchHistoricalData();
 
-
-
-
-
-  },[currencies])
+    /////////////////////////////////////////////////////////////////////////////////////////
 
     
-    // pedir suscripción al servidor
+    console.log(socket.connected); 
+  },[pair])
 
-
-
-
-
-
-  // estto maneja el selector de criptos
+  /////////////////
+  // CRYPT SELECTOR
+  /////////////////
   const handleSelect = (e) => {
-
     // pedir la baja del socket
-    // let unSub = JSON.stringify(unSubMsg);
-    // socket.send(unSub);
+    socket.emit(unSubMsg);
 
-    // setPair cambiará el estado de pair y lanzará el useEffect para una nueva conexion
+    // setPair cambiará el estado de pair y lanzará el useEffect para una nueva petición de criptos
     setPair(e.target.value);
+  };
+  /////////////////
+
+
+  const handleAskForConnect = () => {
+    setInfo(socket.connected ? "I'm connected": "I'm not connected") // true
   };
 
     return (
@@ -198,6 +165,8 @@ const App = () => {
             );
           })}
         </select>
+        <button onClick={handleAskForConnect}>¿Estoy conectado?</button>
+        <h2>{info}</h2>
         <Footer/>
       </div>
     );
